bc594c290a54126d397fe818518a4b660c95cfbe
KianReynolds <S00237055@atu.ie>
2025-04-01 13:41:18 +0100

    Week 6 Lab Setup S00237055

---
+++ RAD302Week6BlazorClient2025.S00237055/SupplierStaticContext.cs
@@ -0,0 +1,24 @@
+﻿using Rad302Week6Lab2025.DataModel.S00237055;
+using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+
+namespace RAD302Week6BlazorClient2025.S00237055
+{
+    public class SupplierStaticContext
+    {
+        public static Supplier CurrentSupplier { get; set; }
+        public static Product[] Products { get; set; }
+        public static Supplier[] Suppliers { get; set; }
+        public static Supplier GetNewSupplier(int supplierID) 
+        {
+            return CurrentSupplier = Suppliers.FirstOrDefault(s => s.SupplierID == supplierID);
+        }
+
+
+    }
+}
