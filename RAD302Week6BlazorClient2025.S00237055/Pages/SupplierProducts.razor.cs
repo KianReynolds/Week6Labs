faf516cbad9d643421ad30e8170b41ecedb27b0c
KianReynolds <S00237055@atu.ie>
2025-04-01 16:22:22 +0100

    Week 6 Lab Supplier Product View setup and lab finished S00237055

---
+++ RAD302Week6BlazorClient2025.S00237055/Pages/SupplierProducts.razor.cs
@@ -0,0 +1,25 @@
+﻿using DataModel;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RAD302Week6BlazorClient2025.S00237055.Pages
+{
+    public partial class SupplierProducts
+    {
+        [Parameter]
+        public int SupplierID { get; set; }
+        public Supplier Current { get; set; }
+
+
+
+        protected override Task OnInitializedAsync() 
+        {
+            Current = SupplierStaticContext.GetNewSupplier(SupplierID);
+
+            return base.OnInitializedAsync();
+        }
+    }
+}
